1a9785899cbd9fba87f661ea95faed010cdb2e64
AustejaCiulkinyte <165672454+AustejaCiulkinyte@users.noreply.github.com>
2024-04-01 17:42:55 +0100

    Update Analysis_4_EFA.R

--- Analysis_4_EFA.R
+++ Analysis_4_EFA.R
@@ -8,7 +8,8 @@ Ssmooth<-as.matrix((nearPD(LDSCoutput$S, corr = FALSE))$mat)
 #run EFA with promax rotation and using the factanal function in the stats package
 require(stats)
 
-#change number of factors incrementally
+#change number of factors incrementally. We provide EFA output ranging from 1 to 6 factors in a separate file
 EFA<-factanal(covmat = Ssmooth, factors = 6, rotation = "promax")
 
+#output results to console
 EFA
