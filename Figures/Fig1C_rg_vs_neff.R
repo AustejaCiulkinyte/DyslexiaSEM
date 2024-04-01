f73ecb4ac12630f57d2e0edcea4e4f7930af1656
AustejaCiulkinyte <165672454+AustejaCiulkinyte@users.noreply.github.com>
2024-04-01 18:18:15 +0100

    Update Fig1C_rg_vs_neff.R

--- Figures/Fig1C_rg_vs_neff.R
+++ Figures/Fig1C_rg_vs_neff.R
@@ -4,16 +4,29 @@ library(ggrepel)
 library(tidyverse)
 library(scales)
 
-# load files
+## load files for plotting
 
 # neff values for each dataset
 neff <- read.csv("Neff.csv", header=TRUE)
-# full correlation matrix
+  #    Name       Neff
+  #1  ADHD 103135.500
+  #2    AN  46321.904
+  #3   ANX 248239.000
+  #4   AUT  41457.380
+  #5   BIP 101574.300
+  #6   DYX 113887.000
+  #7   MDD 430423.800
+  #8   OCD   7281.307
+  #9   SCZ  58746.070
+  #10   TS  12140.028
+
+# full correlation matrix from Supplementary Table 2
 corr <- read.csv("correlations_full.csv", header=TRUE, row.names=1)
-# a dataframe containing names of traits in all possible pairs, but otherwise empty
+
+# a dataframe containing names of traits in all possible pairs, but otherwise empty (45 pairs for 10 traits)
 df <- read.csv("pairs.csv", header=TRUE)
 
-# process file
+# convert variables
 corr <- as.matrix(corr)
 t(corr)
 corr <- as.vector(t(corr))
@@ -23,14 +36,14 @@ corr <- as.vector(t(corr))
 corr[corr==1] <- NA
 corr <- na.omit(corr)
 
-# add pairwise correlation values to the empty dataframe
+# add pairwise correlation values to the empty dataframe with all possible pairs
 df$corr <- corr
 
-# get neff for the first member on each pair in the empty dataframe
+# get neff for the first member of each pair
 Neff1 <- merge(df, neff, by.x="d1", by.y="Name", sort=FALSE)$Neff
 df$Neff1 <- Neff1
 
-# get neff for the second member on each pair in the empty dataframe
+# get neff for the second member of each pair
 df <- merge(df, neff, by.x="d2", by.y="Name", sort=FALSE)
 
 # clean up the dataframe
@@ -49,12 +62,11 @@ modsum = summary(mod1)
 modsum
 
 # define outliers, i.e., which points should be labelled
-#
 # here, points where rg is >0.3 or <0.1, or where pairwise effective Neff is
 # >100,000 will be labelled
 df_outliers <- df %>% filter((corr > 0.3 | corr < 0.1 | sqrtNeff > 100000))
 
-# save the plot as .png
+# save the plot as .pdf
 pdf(file="rg_vs_neff.pdf", width=5, height=5)
 
 df%>%
